62d76ad0f43c969f18e7f0f4b629fc1c090a6b67
JulioGularte <juliogulartedev@gmail.com>
2023-11-15 18:16:39 -0300

    feat: Libreria y funciones para menus

---
+++ menu.h
@@ -0,0 +1,18 @@
+#ifndef MENUS_H_INCLUDED
+#define MENUS_H_INCLUDED
+typedef struct {
+    char *titulo;
+    int cantidadOpciones;
+    int opcionSeleccionada;
+    char *opciones[];
+}Menu;
+
+int gestionarMenu(Menu menu);
+int cambiarOpcionSeleccionada(int teclaPulsada, int cantidadOpciones, int opcionSeleccionada);
+void pintarPantallaConMenu(Menu menu);
+void mostrarPantallaConMenu(Menu menu);
+void mostrarOpcion(char *opcion, int numeroDeOpcion, int opcionSeleccionada);
+
+
+
+#endif // MENUS_H_INCLUDED
